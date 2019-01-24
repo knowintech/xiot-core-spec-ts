4c8be3affcdbd913c20948987db66d386baac378
ouyang <ouyangchengfeng@xiaomi.com>
2019-01-24 16:43:21 +0800

    DeviceHelper: add updateService/updateAction/updateEvent/updateProperty

--- src/xiot/spec/definition/urn/Extendable.ts
+++ src/xiot/spec/definition/urn/Extendable.ts
@@ -7,6 +7,8 @@ export class Extendable {
     public _action_addable: boolean = false;
     public _event_addable: boolean = false;
     public _just_added: boolean = false;
+    public _changed: boolean = false;
     public _active: boolean = false;
     public _checked: boolean = false;
+    public _index_of_added: number = 0;
 }
--- src/xiot/spec/utils/DeviceHelper.ts
+++ src/xiot/spec/utils/DeviceHelper.ts
@@ -1,13 +1,133 @@
-
 import {Device} from '../instance/Device';
 import {Service} from '../instance/Service';
 import {Property} from '../instance/Property';
 import {Action} from '../instance/Action';
 import {Event} from '../instance/Event';
-import {Spec} from '../constant/Spec';
+import {UrnStyle} from '../definition/urn/UrnStyle';
 
 export class DeviceHelper {
   
+    static updateService(service: Service, groupId: string, model: string, version: number): void  {
+      if (service.type != null) {
+        service.type.groupId = groupId;
+        service.type.model = model;
+        service.type.version = version;
+        service.type.style = UrnStyle.XIOT;
+        service.getProperties().forEach(p => DeviceHelper.updateProperty(p, groupId, model, version));
+        service.getActions().forEach(a => DeviceHelper.updateAction(a, groupId, model, version));
+        service.getEvents().forEach(e => DeviceHelper.updateEvent(e, groupId, model, version));
+      }
+    }
+
+    static updateProperty(p: Property, groupId: string, model: string, version: number): void  {
+      if (p.definition != null) {
+        if (p.definition.type != null) {
+          p.definition.type.groupId = groupId;
+          p.definition.type.model = model;
+          p.definition.type.version = version;
+          p.definition.type.style = UrnStyle.XIOT;
+        }
+      }
+    }
+
+    static updateAction(a: Action, groupId: string, model: string, version: number): void  {
+      if (a.type != null) {
+        a.type.groupId = groupId;
+        a.type.model = model;
+        a.type.version = version;
+        a.type.style = UrnStyle.XIOT;
+      }
+    }
+
+    static updateEvent(a: Event, groupId: string, model: string, version: number): void  {
+      if (a.type != null) {
+        a.type.groupId = groupId;
+        a.type.model = model;
+        a.type.version = version;
+        a.type.style = UrnStyle.XIOT;
+      }
+    }
+
+    static alreadyAddedItems(device: Device): boolean  {
+      for (let s of device.getServices()) {
+        if (s.type != null) {
+          if (s.type._just_added) {
+            return true;
+          }
+        }
+      }
+
+      for (let s of device.getServices()) {
+        for (let p of s.getProperties()) {
+          if (p.definition != null) {
+            if (p.definition.type != null) {
+              if (p.definition.type._just_added) {
+                return true;
+              }
+            }
+          }
+        }
+
+        for (let a of s.getActions()) {
+          if (a.type != null) {
+            if (a.type._just_added) {
+              return true;
+            }
+          }
+        }
+
+        for (let e of s.getEvents()) {
+          if (e.type != null) {
+            if (e.type._just_added) {
+              return true;
+            }
+          }
+        }
+      }
+
+      return false;
+    }
+
+    static alreadyChanged(device: Device): boolean  {
+      for (let s of device.getServices()) {
+        if (s.type != null) {
+          if (s.type._just_added || s.type._changed) {
+            return true;
+          }
+        }
+      }
+
+      for (let s of device.getServices()) {
+        for (let p of s.getProperties()) {
+          if (p.definition != null) {
+            if (p.definition.type != null) {
+              if (p.definition.type._just_added || p.definition.type._changed) {
+                return true;
+              }
+            }
+          }
+        }
+
+        for (let a of s.getActions()) {
+          if (a.type != null) {
+            if (a.type._just_added || a.type._changed) {
+              return true;
+            }
+          }
+        }
+
+        for (let e of s.getEvents()) {
+          if (e.type != null) {
+            if (e.type._just_added || e.type._changed) {
+              return true;
+            }
+          }
+        }
+      }
+
+      return false;
+    }
+
     static addServices(device: Device, services: Service[]): void {
       services.forEach(s => DeviceHelper.addService(device, s));
     }
