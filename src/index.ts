241d780295f9e34d19aef901aae86f937319743c
ouyang <jxfengzi@gmail.com>
2019-05-07 16:42:12 +0800

    export all class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,100 @@
 export * from './xiot/core/spec/typedef/constant/Spec';
+
+export * from './xiot/core/spec/typedef/definition/ActionDefinition';
+export * from './xiot/core/spec/typedef/definition/ArgumentDefinition';
+export * from './xiot/core/spec/typedef/definition/DeviceDefinition';
+export * from './xiot/core/spec/typedef/definition/EventDefinition';
+export * from './xiot/core/spec/typedef/definition/PropertyDefinition';
+export * from './xiot/core/spec/typedef/definition/ServiceDefinition';
+
+export * from './xiot/core/spec/typedef/definition/urn/ActionType';
+export * from './xiot/core/spec/typedef/definition/urn/DeviceType';
+export * from './xiot/core/spec/typedef/definition/urn/EventType';
 export * from './xiot/core/spec/typedef/definition/urn/Extendable';
+export * from './xiot/core/spec/typedef/definition/urn/PropertyType';
+export * from './xiot/core/spec/typedef/definition/urn/ServiceType';
+export * from './xiot/core/spec/typedef/definition/urn/Urn';
+export * from './xiot/core/spec/typedef/definition/urn/UrnStyle';
+export * from './xiot/core/spec/typedef/definition/urn/UrnType';
+
+export * from './xiot/core/spec/typedef/definition/property/Access';
+export * from './xiot/core/spec/typedef/definition/property/ConstraintValue';
+export * from './xiot/core/spec/typedef/definition/property/Unit';
+export * from './xiot/core/spec/typedef/definition/property/ValueDefinition';
+export * from './xiot/core/spec/typedef/definition/property/ValueList';
+export * from './xiot/core/spec/typedef/definition/property/ValueRange';
+
+export * from './xiot/core/spec/typedef/definition/property/data/DataFormat';
+export * from './xiot/core/spec/typedef/definition/property/data/DataValue';
+export * from './xiot/core/spec/typedef/definition/property/data/DataValueFactory';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vbool';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vfloat';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vint8';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vint16';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vint32';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vint64';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vstring';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vuint8';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vuint16';
+export * from './xiot/core/spec/typedef/definition/property/data/value/Vuint32';
+
+export * from './xiot/core/spec/typedef/instance/Action';
+export * from './xiot/core/spec/typedef/instance/Argument';
+export * from './xiot/core/spec/typedef/instance/Device';
+export * from './xiot/core/spec/typedef/instance/Event';
+export * from './xiot/core/spec/typedef/instance/Property';
+export * from './xiot/core/spec/typedef/instance/PropertyValue';
+export * from './xiot/core/spec/typedef/instance/Service';
+
+export * from './xiot/core/spec/typedef/operable/ActionOperable';
+export * from './xiot/core/spec/typedef/operable/DeviceOperable';
+export * from './xiot/core/spec/typedef/operable/EventOperable';
+export * from './xiot/core/spec/typedef/operable/PropertyOperable';
+export * from './xiot/core/spec/typedef/operable/ServiceOperable';
+
+export * from './xiot/core/spec/typedef/operation/AbstractOperation';
+export * from './xiot/core/spec/typedef/operation/ActionOperation';
+export * from './xiot/core/spec/typedef/operation/EventOperation';
+export * from './xiot/core/spec/typedef/operation/PropertyOperation';
+
+export * from './xiot/core/spec/typedef/status/OperationStatus';
+
+export * from './xiot/core/spec/typedef/summary/DeviceDetail';
+export * from './xiot/core/spec/typedef/summary/DeviceSummary';
+
+export * from './xiot/core/spec/typedef/udid/XcpUniversalDID';
+
+export * from './xiot/core/spec/typedef/utils/DeviceHelper';
+
+export * from './xiot/core/spec/typedef/xid/AID';
+export * from './xiot/core/spec/typedef/xid/EID';
+export * from './xiot/core/spec/typedef/xid/PID';
+
+export * from './xiot/core/spec/codec/definition/type/ActionTypeCodec';
+export * from './xiot/core/spec/codec/definition/type/EventTypeCodec';
+export * from './xiot/core/spec/codec/definition/type/PropertyTypeCodec';
+export * from './xiot/core/spec/codec/definition/type/ServiceTypeCodec';
+
+export * from './xiot/core/spec/codec/definition/ActionDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/ArgumentDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/DescriptionCodec';
+export * from './xiot/core/spec/codec/definition/DeviceDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/EventDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/PropertyDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/ServiceDefinitionCodec';
+export * from './xiot/core/spec/codec/definition/ValueListCodec';
+export * from './xiot/core/spec/codec/definition/ValueRangeCodec';
+
+export * from './xiot/core/spec/codec/instance/ActionCodec';
+export * from './xiot/core/spec/codec/instance/ArgumentCodec';
+export * from './xiot/core/spec/codec/instance/DeviceCodec';
+export * from './xiot/core/spec/codec/instance/EventCodec';
+export * from './xiot/core/spec/codec/instance/PropertyCodec';
+export * from './xiot/core/spec/codec/instance/ServiceCodec';
+
+export * from './xiot/core/spec/codec/operation/ActionOperationCodec';
+export * from './xiot/core/spec/codec/operation/PropertyOperationCodec';
+
+export * from './xiot/core/spec/codec/summary/DeviceDetailCodec';
+export * from './xiot/core/spec/codec/summary/DeviceSummaryCodec';
+
